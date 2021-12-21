968c7a344949f849842a04f0745c170873b2bf06
bingda <robot.uris@gmail.com>
2021-12-21 23:00:08 +0800

    Add context directory

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,6 @@
 # report the robot's ip address
-ifconfig wlan0 > ../network/wlan0.txt
-git add ../network/wlan0.txt
+cd /home/bingda/repos/robot-operation
+ifconfig wlan0 > network/wlan0.txt
+git add network/wlan0.txt
 git commit -m "Update ip address (`date`)"
 git push
