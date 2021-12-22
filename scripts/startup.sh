80a612ad88c2a270d140b0b9a591626377fed9b4
bingda <robot.uris@gmail.com>
2021-12-22 09:39:49 +0800

    Append uuid

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,7 @@
 # report the robot's ip address
 cd /home/bingda/repos/robot-operation
 ip address > network/ip.txt
+uuidgen | cat >> network/ip.txt
 git add network/ip.txt
 git commit -m "Update ip address (`date`)"
 git push
