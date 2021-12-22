26c8757e75afa953b33f800c9c2839f3d0164e79
bingda <robot.uris@gmail.com>
2021-12-22 10:49:57 +0800

    Change email template

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,2 +1,8 @@
 # Send email
-echo -e "Subject: Robot IP Address \n\n `ip address`" | /usr/sbin/sendmail robot.uris@gmail.com
+(
+echo "From: robot.uris@gmail.com";
+echo "To: robot.uris@gmail.com";
+echo "Subject: IP Address";
+echo "";
+echo "`ip address`"
+) | /usr/sbin/sendmail -t
